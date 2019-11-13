3b4d702d5117956d92ba65105a4eb63e8ced2fa4
Ingrid sura <sura.ingrid@gmail.com>
2019-11-13 21:12:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,6 +2,11 @@ language=['ruby', 'javascript', 'python', 'objective-c']
  def my_collect(collection)
    i = 0 
    empty_collection= []
-   while i < collection.
+   while i < collection.length 
+   empty_collection << yield(collection[i])
+   i += 1
+end
+return empty_collection
+end
   
   
